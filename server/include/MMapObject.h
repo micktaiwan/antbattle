74a933433dc1a31fd3661c1d3336bc397e5d319c
faivrem <faivrem@66201cb4-be1d-0410-8212-75b664826ca3>
2006-09-21 23:37:49 +0000

    changed directories

---
+++ server/include/MMapObject.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+                          mworldsobject.h  -  description
+                             -------------------
+    begin                : Fri Sep 19 2003
+    copyright            : (C) 2003 by Mickael Faivre-Macon
+    email                : mickael@easyplay.com.tw
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MMAPSOBJECTH
+#define MMAPSOBJECTH
+
+#include <string>
+#include "Msg.h"
+
+/**
+  *@author Mickael Faivre-Macon
+  */
+//---------------------------------------------------------------------------
+class Vector {
+public:
+   int X,Y;
+   Vector(int x, int y) : X(x), Y(y) {}
+   };
+
+//---------------------------------------------------------------------------
+class MMapObject {
+public:
+
+
+   unsigned long  ID;
+   Vector         Pos;
+
+   MMapObject();
+   virtual ~MMapObject();
+
+   virtual void DataString(mnetmsg::base&)=0;
+   
+};
+
+#endif
