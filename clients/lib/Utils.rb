aaea0e1de835fda47ee6aa453ca0622a70c0b7a8
faivrem <faivrem@66201cb4-be1d-0410-8212-75b664826ca3>
2006-11-08 22:01:10 +0000

    Broken....

--- clients/lib/Utils.rb
+++ clients/lib/Utils.rb
@@ -1,4 +1,3 @@
-#module Utils
 class String;
 
 def ord; self[0]; end; 
@@ -15,6 +14,7 @@ def last; self.to_s[self.to_s.length-1].to_s; end;
 end
 
    PARAMSEP = "\e"
+   PARAMSEP_INT = 27
    
    def translate_msg(pattern, packet)
      if (!packet.instance_of? Array)
@@ -81,4 +81,83 @@ end
          end
          }
       rv
+   end
+
+   def get_param(m,n)
+      #puts "get_param(#{m},#{n})"
+   
+      n = 1 if(n==0) # 1 based
+
+      i    = 2 # skip the first 2 netmsg category chars
+      ps   = 0 # paramsep counter
+      size = m.size
+      param = ''
+
+      while(i < size)
+         if(m[i]==PARAMSEP_INT)
+            ps+= 1   # we just passed a parameter
+            i += 1    # skip the PARAMSEP
+            return param if(i==size)
+            next
+         end
+         return param if(ps==n)  # that was it, param contains the last param
+         param += m[i].chr if(ps==n-1) # we are reading it
+         i+=1
+      end       
+
+      return param # we read all the string
+   end
+
+   def get_param_from(m,n)
+      #puts "get_param_from(#{m},#{n})"
+      msg = "xx"+m[n..(m.size-1)]
+      get_param(msg,1)
+   end
+   
+   def parse_msg(msg,format)
+      rv = []
+      i = 0
+      format.each_byte { |b|
+         case b.chr
+            when 'b'
+               rv << msg[i]
+            when 's'
+               str = ""
+               begin
+                  str += msg[i].chr
+                  i += 1
+               end while msg[i]!=PARAMSEP_INT
+               rv << str
+            else puts "unknown format"
+         end
+         i += 1
+         }
+      rv
+   end
+   
+   def sanitize(msg)
+      rv = ""
+      s = msg.size
+      i = 0
+      while(i < s)
+         if (msg[i] == 27)
+            rv += '/' 
+         elsif (msg[i] < 32)
+            rv += '-' 
+         else
+            rv += msg[i].chr
+         end
+         i += 1
+      end
+      rv
+   end
+   
+   def deep_copy(obj)
+      Marshal.load(Marshal.dump(obj))
+   end
+   
+   def log level,indent,str
+      return if level > 1
+      indent.times {print " "}
+      puts str
    end
