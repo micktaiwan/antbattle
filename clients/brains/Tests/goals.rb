e0a4e683f5e33309350e1ad946aa8f9993fb63d3
faivrem <faivrem@66201cb4-be1d-0410-8212-75b664826ca3>
2007-07-08 16:28:21 +0000

    Updated mick brain

--- clients/brains/Tests/goals.rb
+++ clients/brains/Tests/goals.rb
@@ -45,75 +45,78 @@ class Ant
    # it's the main "intelligent" function for an ant
    def get_move
       #puts "get_move for #{self}"
-      return [] if goal == nil
-      rv = []
-      case goal[0]
-         when KILL
-            e = goal[1] # get the ennemy
-            if(not $map.exists?(e))
-               puts 'this ant does not exists anymore, doing nothing: not good !!!'
-               return rv 
-            end
-            if(e.life<=0)
-               puts "#{e} is dead, doing nothing: not good !!!"
-               return rv
-            end
-            #TODO: do something else
-            
-            dist = distance(self,e)
-            #puts "I am #{self}"
-            #puts "goal: kill this enneny: #{e}"
-            i = 0
-            if dist > 1 # move
-               # by doing this we are finding subgoals
-               # TODO: add_sub_goal ?
-
-               path = find_best_way(e.x,e.y)# we have to move next to it
-               #puts ppath(path)
-               if path==nil
-                  # TODO: no path, find something else to do
-                  puts 'no path ! doing nothing: not good !!!'
-                  return rv
+      return [] if goal == nil or goal == []
+      while(1)
+         rv = []
+         g = goal.shift
+         return [] if g == nil
+         puts g.join('---')
+         case g[0]
+            when KILL
+               e = g[1] # get the ennemy
+               if(not $map.exists?(e) or e.life<=0)
+                  puts 'this ant does not exists anymore or is dead, next goal'
+                  next 
                end
                
-               # we have a path to it
-               # so now we have to move smartly
-               # if we are at 3 cases, we can move and attack
-               # more than that we have to move at 4 cases and wait the next turn
-               # TODO: do not wait at a position where we can be attacked
-               if(path.size-2 <= 3) # minus two because the path include our case
-                  i = path.size-2  # 0 based minus one, so -2 
-                  #puts 's1'
-               elsif(path.size-2 >= 6) # we are far away
-                  i = 6
-                  #puts 's2'
-               else
-                  i = (path.size-2)
-                  #puts 's3'
+               dist = distance(self,e)
+               #puts "I am #{self}"
+               #puts "goal: kill this enneny: #{e}"
+               i = 0
+               if dist > 1 # move
+                  # by doing this we are finding subgoals
+                  # TODO: add_sub_goal ?
+
+                  path = find_best_way(e.x,e.y)# we have to move next to it
+                  #puts ppath(path)
+                  if path==nil
+                     # TODO: no path, find something else to do
+                     puts 'no path ! , next goal'
+                     next
+                  end
+                  
+                  # we have a path to it
+                  # so now we have to move smartly
+                  # if we are at 3 cases, we can move and attack
+                  # more than that we have to move at 4 cases and wait the next turn
+                  # TODO: do not wait at a position where we can be attacked
+                  if(path.size-2 <= 3) # minus two because the path include our case
+                     i = path.size-2  # 0 based minus one, so -2 
+                     #puts 's1'
+                  elsif(path.size-2 >= 6) # we are far away
+                     i = 6
+                     #puts 's2'
+                  else
+                     i = (path.size-2)
+                     #puts 's3'
+                  end
+                  #puts i
+                  #sleep(1)
+                  if i <= 0
+                     puts "i <= 0 (#{i}), #{ppath(path)}"
+                     sleep(5)
+                  else
+                     a,b = path[i][0]
+                     #puts "I am moving to (#{[a,b].join(',')})"
+                     self.x,self.y = a,b
+                     rv << "Cb#{object_id}~"+[a,b].pack('cc')
+                     #puts x
+                  end
                end
+               return rv if i > 3 # return if we can not attack at the same round
                #puts i
-               #sleep(1)
-               if i <= 0
-                  puts "i <= 0 (#{i}), #{ppath(path)}"
-                  sleep(5)
-               else
-                  a,b = path[i][0]
-                  #puts "I am moving to (#{[a,b].join(',')})"
-                  self.x,self.y = a,b
-                  rv << "Cb#{object_id}~"+[a,b].pack('cc')
-                  #puts x
+               dist = distance(self,e)
+               if dist == 1
+                  puts 'Attack'
+                  rv << "Cc#{object_id}~#{e.object_id}"
+                  e.life -= 5
                end
-            end
-            return rv if i > 3 # return if we can not attack at the same round
-            #puts i
-            dist = distance(self,e)
-            if dist == 1
-               #puts 'Attack'
-               rv << "Cc#{object_id}~#{e.object_id}"
-               e.life -= 5
-            end
-            #sleep(1)
-      end # case
+               return rv
+         else
+            puts 'unknown goal, next goal'
+         end # case
+      end #while
+      puts "no goal ? find something else to do !"
       rv
    end
 
@@ -133,29 +136,25 @@ class Colony
    # set the goals for each ant
    def set_goals
       $map = @map # FIXME
-      # doing simple: give the goal "kill this ant" for each ant
-      dist = []
-      ant = nil
-      @map.allies_each(@id) { |a|
-         ant = a
-         break
-         }
-      @map.ennemies_each(@id) { |e|
-         dist << [e,distance(ant,e)]
-         }
-      dist = dist.sort_by {|couple| couple[1]}
-      #puts dist
-      #sleep(5)
-      i = 1
-      enn = 0
-      @map.allies_each(@id) { |a|
-         break if enn >= dist.size
-         a.goal = [KILL, dist[enn][0]]
-         i += 1 
-         if(i==5 and enn < dist.size-1)
-            enn += 1
-            i = 1
-         end
+      # doing simple: give the goal "kill the nearest ant" for each ant
+      @map.allies_each(@id) { |ant|
+         ant.goal = []
+         # calculate distances
+         dist = []
+         @map.ennemies_each(@id) { |e|
+            dist << [e,distance(ant,e)]
+            }
+         dist = dist.sort_by {|couple| couple[1]}
+         # add the goal "Kill this ant" for each ennemy
+         dist.each { |d|
+            ant.goal << [KILL, d[0]]
+            }
+         # print information
+         puts "#{ant} goals:"
+         ant.goal.each { |g|
+            puts "   "+g.join(': ')            
+            }
+         #break   
          }
       return
 
