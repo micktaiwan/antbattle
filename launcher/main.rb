e8c7356c1f5e687224b028cd6533fe24bd3803ef
faivrem <faivrem@66201cb4-be1d-0410-8212-75b664826ca3>
2008-11-11 00:44:19 +0000

    launcher work

--- launcher/main.rb
+++ launcher/main.rb
@@ -6,23 +6,47 @@ class Gui < GuiGlade
 
   def initialize(path_or_data, root = nil, domain = nil, localedir = nil, flag = GladeXML::FILE)
     super(path_or_data, root , domain , localedir, flag )
+    @glade['stsbar'].push(0,"Starting...")
     tree = @glade['tree']
-    tree.append_column(Gtk::TreeViewColumn.new('Bot name'))
-    @list = Gtk::ListStore.new(String)
+    renderer = Gtk::CellRendererText.new
+    tree.append_column(Gtk::TreeViewColumn.new('Bot name',renderer,:text => 0))
+    tree.append_column(Gtk::TreeViewColumn.new('Version',renderer,:text => 1))
+    @list = Gtk::ListStore.new(String,String)
     tree.set_model(@list)
-   
     load_bots
   end
 
-  def load_bots
+  def add_bot(name, version)
     n = @list.append
-    n.set_value(0,"yqsdo")
+    n.set_value(0,name)
+    n.set_value(1,version)
+  end
+
+  def load_bots
+    @glade['stsbar'].push(0,"Loading bots...")
+    add_bot('Random','1.0')
+    @glade['stsbar'].push(0,"Bots loaded")
+  end
+  
+  def draw
+      area = @glade['field']
+      alloc = area.allocation
+      #area.window.draw_arc(area.style.fg_gc(area.state), true, 
+      #                 0, 0, alloc.width, alloc.height, 0, 64 * 360)
+      for y in (0..20) 
+        area.window.draw_line(area.style.fg_gc(area.state),10,y*20+10,410,y*20+10)
+      end
+      for x in (0..20) 
+        area.window.draw_line(area.style.fg_gc(area.state),x*20+10,10,x*20+10,410)
+      end
   end
   
   def on_main_destroy(widget)
     Gtk.main_quit
   end
-  
+  def on_field_expose_event(widget, arg0)
+    draw
+  end
 end
 
 
